0337ee225fdbae1e8816efc31a439c50e19c267e
lsert <zqc200789789@126.com>
2020-03-27 19:09:16 +0800

    修改一些样式

--- components/button/button.tsx
+++ components/button/button.tsx
@@ -1,4 +1,4 @@
-import React, { ButtonHTMLAttributes } from 'react';
+import React, { ButtonHTMLAttributes, useCallback, useState, MouseEvent } from 'react';
 import classnames from 'classnames';
 
 interface PropsIF extends ButtonHTMLAttributes<HTMLButtonElement> {
@@ -7,9 +7,21 @@ interface PropsIF extends ButtonHTMLAttributes<HTMLButtonElement> {
 
 export function Button(props: PropsIF) {
   const { className, children, ...others } = props;
+  const [showWave, setShowWave] = useState(false);
   const clsObj = classnames({
     'ls-btn': true,
     [`${className}`]: !!className,
+    'ls-wave': showWave
   })
-  return <button className={clsObj} {...others}>{children}</button>
+  const onMouseDown = useCallback((e: MouseEvent<HTMLButtonElement>) => {
+    console.log(e.pageX);
+    console.log(e.pageY);
+    setShowWave(true);
+  }, []);
+  return (
+    <button className={clsObj} {...others} onMouseDown={onMouseDown}>
+      {children}
+      {showWave ? <div className="ls-wave-content"></div> : null}
+    </button>
+  )
 }
--- src/Input/Home.tsx
+++ src/Input/Home.tsx
@@ -1,4 +1,4 @@
-import React, { memo, useEffect, useState, ChangeEvent } from 'react';
+import React, { memo, useEffect, useState, ChangeEvent, MouseEvent } from 'react';
 import { useLocalStore, useObserver, Observer, observer } from 'mobx-react';
 import { Input } from 'components/input';
 import { Button } from 'components/button';
@@ -25,6 +25,19 @@ export function Home() {
         const { data } = pageData;
         if (data) {
           data.projectContext = e.currentTarget.value;
+        }
+      },
+      addPlanList: (e: MouseEvent<HTMLButtonElement>) => {
+        const { data } = pageData;
+        if (data) {
+          data.planList.push({});
+        }
+      },
+      deletePlan: (e: MouseEvent<HTMLButtonElement>) => {
+        const { index } = e.currentTarget.dataset;
+        const { data } = pageData;
+        if (data) {
+          data.planList.splice(index, 1);
         }
       }
     }
@@ -55,10 +68,18 @@ export function Home() {
           onChange={pageData.onValueChange}
         />
         <br />
+        {
+          data.planList.map((item, index) => {
+            return <div>
+              <Input value={item.planFullName} />
+              <Button data-index={index} onClick={pageData.deletePlan}>删除</Button>
+            </div>
+          })
+        }
         <br />
-      产品
+        产品
         <br />
-        <Button>点击我</Button>
+        <Button onClick={pageData.addPlanList}>点击我</Button>
       </div>
     )
   });
